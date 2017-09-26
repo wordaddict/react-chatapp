a73f4137fe20093f205ae9c801a357fd642207ce
wordaddict <madeyinka6@gmail.com>
2017-09-26 22:00:31 +0100

    Emit newMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Micheal',
-    text: 'Really'
-  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -15,18 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: "Tosin",
-    text: "I love you guys",
-    createdAt: 8
-  })
-
-  socket.on('createEmail', (newEmail) => {
-    console.log('createEmail', newEmail);
-  })
-
   socket.on('createMessage', (message) => {
     console.log('Message created', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   });
 
   socket.on('disconnect', () => {
